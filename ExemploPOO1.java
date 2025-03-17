6c0044a16a079c40ced6f569aa709c50486cafa1
Maria Eduarda <136113549+dudamerloc@users.noreply.github.com>
2025-03-17 10:06:11 -0300

    Update ExemploPOO1.java

--- ExemploPOO1.java
+++ ExemploPOO1.java
@@ -1,15 +1,22 @@
+/*o que é uma classe? 
+Uma classe é um modelo (molde) que define atributos (características) e métodos (ações) de um objeto.
+Imagine que uma classe seja um "molde de bolo". 
+Com esse molde, podemos fazer vários bolos (objetos) iguais, mas com recheios diferentes.*/
+
 class Carro{ //Classe
     String modelo;
     int ano;
 
     void buzinar(){ //Método
         System.out.println("Bii Bii!");
-
-
     }
 }
 
 /*A classe Carro define o que um carro deve ter (modelo e ano) e o que ele pode fazer (buzinar()) */
+
+/*O que é um Objeto? 
+ * Um objeto é uma instância de uma classe. Ele representa um exemplo real do que foi definido no molde. 
+ * Se a Classe Carro é um molde do bolo, o objeto meuCarro é um bolo feito com esse molde. */
 public class ExemploPOO1 {
     public static void main(String[] args) {
         // Criando um objeto da classe carro
@@ -28,4 +35,3 @@ public class ExemploPOO1 {
         
     }
 }
-
